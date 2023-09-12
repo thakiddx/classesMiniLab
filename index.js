bba4a0c0efa3b78dd51cb03e92792009fe53281b
xmalone0920 <xmalone0920@gmail.com>
2023-09-12 14:04:12 -0700

    completed

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ class  User {
    }
 }
 
+const Xavier = new User('g_unitsoulja206@msn.com', 'g_unitSoulja', '12345');
+const Unity = new User('meanGirl21@aol.com', 'meanGirl21', '345212');
+const Quentin = new User('coolBreeze92@yahoo.com', 'coolBreeze92', '231456');
+
+
+console.log(Xavier);
+console.log(Unity);
+console.log(Quentin);
